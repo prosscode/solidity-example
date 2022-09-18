b3a77635fb309b2c8c3de8d51a87d2a270b4d2f0
prosscode <rukiapross@foxmail.com>
2022-09-19 00:47:42 +0800

    add readme

--- 24_Fallback.sol
+++ 24_Fallback.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
+// 回退函数
 // fallback executed then
 // - function does not exist
 // - directly send ETH 
--- README.md
+++ README.md
@@ -1,1 +1,8 @@
+<!--
+ * @Description: 
+ * @Author: pross
+ * @Date: 2022-09-16 17:32:34
+-->
 # solidity-example
+
+learning case for solidity 0.8^
