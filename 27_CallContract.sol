fa174b85e1ab1a0a19c6e333d818ccfb19310242
prosscode <rukiapross@foxmail.com>
2022-09-28 00:11:48 +0800

    add interface and abi singnature method

--- 27_CallContract.sol
+++ 27_CallContract.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract CallTestContract{
+contract CallContract{
 
     function setX(address _test,uint _x) external{
         TestContract(_test).setX(_x);
